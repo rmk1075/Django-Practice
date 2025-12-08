faf972b8c9105613b689378412a58bb5c4a36f94
jamie <rmk1075@gmail.com>
2025-12-08 23:52:37 +0900

    feat: add new model 'Greeting'

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Greeting(models.Model):
+    country = models.CharField(max_length=100)
+    greeting = models.CharField(max_length=100)
