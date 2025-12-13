4f2ca1883098db4f37b9d428c8da85b716c8f1e7
jamie <rmk1075@gmail.com>
2025-12-13 23:46:00 +0900

    test: fix test code

--- hello/tests/test_models.py
+++ hello/tests/test_models.py
@@ -18,15 +18,16 @@ class TestModels(TestCase):
     def test_greeting_crud(self):
         # create objects
         for k, v in GREETINGS_MAP.items():
+            # Greeting.objects.create(country=k, greeting=v)
             greeting = Greeting(country=k, greeting=v)
             greeting.save()
 
         # retrieve objects
         greetings = Greeting.objects.all()
-        self.assertEqual(len(greetings), len(greetings_map))
+        self.assertEqual(len(greetings), len(GREETINGS_MAP))
 
         for greeting in greetings:
-            self.assertEqual(greeting.greeting, greetings_map.get(greeting.country))
+            self.assertEqual(greeting.greeting, GREETINGS_MAP.get(greeting.country))
 
         # update objects
         hello = Greeting.objects.get(country="US")
