4cfe75431a5a5838a29c86c09f4ecdd481a45fdb
jamie <rmk1075@gmail.com>
2025-12-26 19:50:31 +0900

    feat: add FileSystem Cache - define default and file_cache - add test code for FileSystem Cache

--- myproject/settings/test.py
+++ myproject/settings/test.py
@@ -1,3 +1,24 @@
 from myproject.settings.base import *
 
 SETTINGS_MODE = "TEST"
+
+"""
+- MAX_ENTRIES:  The maximum number of entries allowed in the cache before old values are deleted. This argument defaults to 300.
+- CULL_FREQUENCY:  The fraction of entries that are culled when MAX_ENTRIES is reached. The actual ratio is 1 / CULL_FREQUENCY. This argument should be an integer and defaults to 3.
+"""
+FILE_CACHE_MAX_ENTRIES = 300
+FILE_CACHE_CULL_FREQUENCY = 3
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache"
+    },
+    "file_cache": {
+        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+        "LOCATION": BASE_DIR / "django_cache" / "file_cache",
+        "OPTIONS": {
+            "MAX_ENTRIES": FILE_CACHE_MAX_ENTRIES,
+            "CULL_FREQUENCY": FILE_CACHE_CULL_FREQUENCY,
+        }
+    }
+}
--- myproject/tests/test_caching.py
+++ myproject/tests/test_caching.py
@@ -1,5 +1,6 @@
 import random
-from django.core.cache import cache
+from django.conf import settings
+from django.core.cache import cache, caches
 from django.test import TestCase
 
 
@@ -13,4 +14,40 @@ class TestSettings(TestCase):
         for k, v in enumerate(l):
             self.assertEqual(cache.get(k), v)
 
+    def test_filesystem_cache(self):
+        l = [random.randint(0, 100) for _ in range(100)]
 
+        cache = caches['file_cache']
+        cache.clear()
+
+        for k, v in enumerate(l):
+            cache.set(k, v)
+
+        for k, v in enumerate(l):
+            self.assertEqual(cache.get(k), v)
+
+        cache.clear()
+
+    def test_filesystem_cache_entry(self):
+        size = 300
+        l = [random.randint(0, 100) for _ in range(size)]
+
+        cache = caches['file_cache']
+        cache.clear()
+
+        for k, v in enumerate(l):
+            cache.set(k, v)
+
+        for k, v in enumerate(l):
+            self.assertEqual(cache.get(k), v)
+
+        cache.set(size + 1, random.randint(0, 100))
+
+        number_of_none = 0
+        for k, v in enumerate(l):
+            if cache.get(k) is None:
+                number_of_none += 1
+
+        self.assertEqual(number_of_none, int((size + 1) / settings.FILE_CACHE_CULL_FREQUENCY))
+
+        cache.clear()
