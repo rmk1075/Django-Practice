cdb04a7e6726867ec3b3e430991ebfcb22e0b51f
jamie <rmk1075@gmail.com>
2025-12-27 23:00:06 +0900

    test: write test code for loggers - console debug logger - file debug logger

--- myproject/tests/test_logging.py
+++ myproject/tests/test_logging.py
@@ -1,15 +1,42 @@
 import logging
+import os
+from django.conf import settings
 from django.test import TestCase
 
 
 logger = logging.getLogger(__name__)
+console_debug_logger = logging.getLogger("console_debug")
+file_debug_logger = logging.getLogger("file_debug")
 
 class TestLoggings(TestCase):
-    def test_logger(self):
-        logger.debug("debug")
-        logger.info("info")
+    def setUp(self):
+        with open(settings.DEBUG_LOG_FILE, "w"):
+            pass
+
+    def test_root_logger(self):
+        logger.debug("debug") # skipped
+        logger.info("info") # skipped
         logger.warning("warning")
         logger.error("error")
         logger.critical("critical")
-        
-        
+
+    def test_console_debug_logger(self):
+        console_debug_logger.debug("debug")
+        console_debug_logger.info("info")
+        console_debug_logger.warning("warning")
+        console_debug_logger.error("error")
+        console_debug_logger.critical("critical")
+
+    def test_file_debug_logger(self):
+        file_debug_logger.debug("debug")
+        file_debug_logger.info("info")
+        file_debug_logger.warning("warning")
+        file_debug_logger.error("error")
+        file_debug_logger.critical("critical")
+
+        with open(settings.DEBUG_LOG_FILE, "r") as f:
+            self.assertEqual(f.readline().strip(), "debug")
+            self.assertEqual(f.readline().strip(), "info")
+            self.assertEqual(f.readline().strip(), "warning")
+            self.assertEqual(f.readline().strip(), "error")
+            self.assertEqual(f.readline().strip(), "critical")
