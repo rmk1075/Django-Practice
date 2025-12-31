286f0f795b63549bb09d1e3d7ae396396d76d89d
jamie <rmk1075@gmail.com>
2025-12-31 14:20:33 +0900

    feat: create Greeting feature - add POST /hello API - modify hello.html   - add new Greeting area

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import redirect, render
 from django.views import View
 
 from hello.models import GREETINGS_MAP, Greeting
@@ -21,7 +21,26 @@ def init_greetings(request):
 
     return HttpResponse(content=created_greetings)
 
+
+class PostHelloRequest:
+    def __init__(self, request):
+        self.country = request.POST.get("country")
+        self.greeting = request.POST.get("greeting")
+
+
 class HelloView(View):
-    def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs) -> HttpResponse:
         result = [{"country": greeting.country, "greeting": greeting.greeting} for greeting in Greeting.objects.all()]
         return render(request, "hello.html", {"result": result})
+
+    def post(self, request, *args, **kwargs) -> JsonResponse:
+        post_hello_request = PostHelloRequest(request)
+        obj, created = Greeting.objects.get_or_create(
+            country=post_hello_request.country,
+            defaults={"greeting": post_hello_request.greeting}
+        )
+
+        message = f"[country={obj.country} greeting={obj.greeting}]"
+        message += " is created." if created else " already exists."
+        result = [{"country": greeting.country, "greeting": greeting.greeting} for greeting in Greeting.objects.all()]
+        return render(request, "hello.html", {"result": result, "created": message})
