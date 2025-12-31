f4bc981283cb8153236f37033b6ed0e2751ed182
jamie <rmk1075@gmail.com>
2025-12-31 12:09:37 +0900

    feat: add GET /hello API - implements HelloView - add template hello.html

--- hello/urls.py
+++ hello/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.HelloView.as_view(), name="hello"),
     path("world", views.hello_world, name="world"),
     path("init", views.init_greetings, name="init")
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
 
 from hello.models import GREETINGS_MAP, Greeting
 
@@ -18,3 +20,8 @@ def init_greetings(request):
             created_greetings.append({"country": obj.country, "greeting": obj.greeting})
 
     return HttpResponse(content=created_greetings)
+
+class HelloView(View):
+    def get(self, request, *args, **kwargs):
+        result = [{"country": greeting.country, "greeting": greeting.greeting} for greeting in Greeting.objects.all()]
+        return render(request, "hello.html", {"result": result})
